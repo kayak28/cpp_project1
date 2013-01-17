5c8326a726a3747dd4cf872d54fc5f3b2add913c
28kayak <kaya_ota@msn.com>
2013-01-16 16:29:48 -0800

    initial commit

---
+++ Cprogram1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	double x; 
+	double y;
+	
+	cout << "entry variable x = ";
+	cin >> x;
+
+	cout << "entry variable y = ";
+	cin >> y;
+
+	cout << "x + y = " << x+y << "\n";
+	
+	return 0;
+
+}
